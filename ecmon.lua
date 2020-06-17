1dac4256d396ab3d21e87e8e55c6369141c7939c
Sirnut <67026260+Sirnut@users.noreply.github.com>
2020-06-17 17:41:40 -0500

    Update ecmon.lua

--- ecmon.lua
+++ ecmon.lua
@@ -73,7 +73,9 @@ function update()
 			else
   			fluxgate.setSignalLowFlow(1000000)
 			end
+			
 			energyPercent = math.ceil(energy / energycore.getMaxEnergyStored() * 10000)*.01
+			
 			f.draw_text_lr(mon, 2, 3, 1, "Core Storage", energyPercent .. "%", colors.white, colors.lime, colors.black)
 	  	f.progress_bar(mon, 2, 4, mon.X-2, energyPercent, 100, colors.cyan, colors.gray)
 		end
